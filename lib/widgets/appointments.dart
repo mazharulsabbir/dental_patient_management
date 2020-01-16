d36d5392cbf5f2d43070f78d2a4cf49324e99de2
mazharulsabbir <mazharulsabbir2@gmail.com>
2020-01-16 21:41:15 +0600

    tab bar layout initiated

--- lib/widgets/appointments.dart
+++ lib/widgets/appointments.dart
@@ -8,18 +8,42 @@ class DoctorAppointment extends StatefulWidget {
 class _DoctorAppointmentState extends State<DoctorAppointment> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Appointments'),
-        centerTitle: true,
-        leading: IconButton(
-          icon: Icon(Icons.ac_unit),
-          onPressed: () {
-            print('Unit Pressed');
-          },
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Appointments'),
+          centerTitle: true,
+          leading: IconButton(
+            icon: Icon(Icons.ac_unit),
+            onPressed: () {
+              print('Unit Pressed');
+            },
+          ),
+          
+          bottom: TabBar(
+            isScrollable: true,
+            indicator: BoxDecoration(
+              borderRadius: BorderRadius.only(topLeft: Radius.circular(8.0),topRight: Radius.circular(8.0)),
+              color: Colors.redAccent
+            ),
+            tabs: <Widget>[
+              Tab(
+                text: 'Upcoming',
+              ),
+              Tab(
+                text: 'Past',
+              )
+            ],
+          ),          
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            Icon(Icons.local_offer),
+            Icon(Icons.receipt),
+          ],
         ),
       ),
-      
     );
   }
 }
