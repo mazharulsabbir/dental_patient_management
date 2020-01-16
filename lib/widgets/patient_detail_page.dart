15a928d6b6f6fcb51e4104e51a46b947b003e4b5
mazharulsabbir <mazharulsabbir2@gmail.com>
2020-01-16 19:38:44 +0600

    collapsing toolbar initiated

--- lib/widgets/patient_detail_page.dart
+++ lib/widgets/patient_detail_page.dart
@@ -8,35 +8,28 @@ class PatientDetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(
-          _patient.name,
-        ),
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxScroll) {
+          return <Widget>[
+            SliverAppBar(
+              expandedHeight: 200.0,
+              floating: false,
+              pinned: true,
+              flexibleSpace: FlexibleSpaceBar(
+                  centerTitle: true,
+                  title: Text(
+                    _patient.name,
+                  ),
+                  background: Image.asset(
+                    "man.png",
+                    fit: BoxFit.cover,
+                  )
+                  ),
+            )
+          ];
+        },
+        body: DetailOfPatient(_patient),
       ),
-      // body: NestedScrollView(
-      //   headerSliverBuilder: (BuildContext context, bool innerBoxScroll) {
-      //     return <Widget>[
-      //       SliverAppBar(
-      //         expandedHeight: 200.0,
-      //         floating: false,
-      //         pinned: true,
-      //         flexibleSpace: FlexibleSpaceBar(
-      //           centerTitle: true,
-      //           title: Text(
-      //             _patient.name,
-      //           ),
-      //           // background: Image.asset(
-      //           //   "man.png",
-      //           //   fit: BoxFit.cover,
-      //           // )
-      //         ),
-      //       )
-      //     ];
-      //   },
-      //   body: DetailOfPatient(_patient),
-      // ),
-      body: DetailOfPatient(_patient),
     );
   }
 }
